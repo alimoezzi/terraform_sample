7ae9d7babeb256b136d192847b9db39efc1f3ac4
ALi <s.arme@ya.ru>
2021-03-10 15:37:36 +0300

    Adopt default vpc

--- prod-us-east-2.tf
+++ prod-us-east-2.tf
@@ -3,7 +3,12 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "tf_sample_aws_bucket" {
+resource "aws_s3_bucket" "prod_tf_sample_aws_bucket" {
   bucket = "sarme-tf-sample-2021"
   acl = "private"
 }
+
+resource "aws_default_vpc" "default" {
+  
+}
+
